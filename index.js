e06ac1c9049b7320e27729b784b28b351dcae495
sherifnada <snadalive@gmail.com>
2016-01-05 23:48:46 -0500

    Added initial script that tunes into random game and downloads all spectator packets to completion

--- index.js
+++ index.js
@@ -1,45 +1,87 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var fs = require('fs');
+
 var regionPrefix = "http://spectator.na.lol.riotgames.com/observer-mode/rest";
-var getLastChunkInfo = "/consumer/getLastChunkInfo"; //<platformId>/<gameID>/1/token"
-// var gameID = "";
-// var platformID = "NA1";
+var featuredGamesUrl = regionPrefix + "/featured";
+var getLastChunkInfo = "consumer/getLastChunkInfo"; //<platformId>/<gameID>/1/token"
+var getGameDataChunk = "consumer/getGameDataChunk";
+var getKeyFrame = "consumer/getKeyFrame";
+var lastChunkId; 
+var lastKeyFrameId;
 
-var getJson = function(url){
-  return new Promise(function(resolve, reject){
+var getJson = function(url){  
     var request = new XMLHttpRequest();
-    
-    console.log('created request');
-
-    request.addEventListener("load", function() {
-      if (request.status >= 200 && request.status < 400) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    });
-
-    request.addEventListener("error", function(event) {
-        reject(Error("Network error"));
-    });
-
     request.responseType = 'json';
-    request.open('GET',url, true);
-    console.log('Request Opened');
-
+    request.open('GET',url, false);
     request.send();
-    console.log('Request sent');
-  });
+    return JSON.parse(request.responseText);
 };
 
-function saveMatchPackets(platformId, gameId){
-    //get packet Info, parse JSON
-    var url = regionPrefix + "/" + getLastChunkInfo + "/" + platformId + "/" + gameId + "/1/token"; 
-    getJson(url)
-    .then(function(response){
-        console.log(response);
-    }).catch(function(error){
-        console.log(error.message);
-    });
+var getResponse = function(url){
+    var request = new XMLHttpRequest();
+    request.open('GET',url, false);
+    request.send();
+    return request.responseText;
 }
 
-saveMatchPackets("NA1", "2059897698");
+function getRandomGameId(){
+    return getJson(featuredGamesUrl)['gameList'][0]['gameId'];
+}
 
+var mkdirSync = function (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    if ( e.code != 'EEXIST' ) throw e;
+  }
+}
+
+function saveGameChunks(platformId, gameId){
+    
+    //get chunk Id
+    var url = regionPrefix + "/" + getLastChunkInfo + "/" + platformId + "/" + gameId + "/1/token"; 
+    var chunkInfo = getJson(url);
+    var chunkId = chunkInfo['chunkId'];
+    var keyFrameId = chunkInfo['keyFrameId'];
+    var endGameChunkId = chunkInfo['endGameChunkId'];
+
+    if (lastChunkId !== chunkId){
+        console.log("CHUNK ID: " + chunkId);
+
+        lastChunkId = chunkId;
+        url = regionPrefix + "/" + getGameDataChunk + "/" + platformId + "/" + gameId + "/" + chunkId + "/token"; 
+        var chunk = String(getResponse(url));
+        mkdirSync("./replays/" + gameId + "/");
+        fs.writeFile("./replays/" + gameId + "/" + chunkId, chunk, function(err, data){
+            if (err){
+                console.log("Unable to write chunk to disk. ");
+                console.log(err);
+            }
+        });
+    }
+
+    if (keyFrameId !== lastKeyFrameId){
+        console.log("KEYFRAME ID: " + keyFrameId);
+        lastKeyFrameId = keyFrameId;
+        url = regionPrefix + "/" + getKeyFrame + "/" + platformId + "/" + gameId + "/" + chunkId + "/token"; 
+        var keyFrame = String(getResponse(url));
+        mkdirSync("./replays/" + gameId + "/");
+        fs.writeFile("./replays/" + gameId + "/" + chunkId + "kf", chunk, function(err, data){
+            if (err){
+                console.log("Unable to write chunk to disk. ");
+                console.log(err);
+            }
+        });
+    }
+
+    //if game has ended, exit
+    if (endGameChunkId && +endGameChunkId !== 0){
+        console.log("EXITING");
+        process.exit();
+    }
+}
+
+var gameId = getRandomGameId();
+console.log("GAME ID: " + gameId);
+//check for new chunk every 10 seconds
+setInterval( function(){ saveGameChunks("NA1", gameId); } , 10000);
